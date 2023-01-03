53961827ce7395008b9296d09e143103c9d58331
Anton Saltanov <saltanov.anton@yandex.ru>
2023-01-03 11:35:19 +0900

    task 2

--- src/Car.java
+++ src/Car.java
@@ -1,10 +1,19 @@
 public class Car {
-    String brand;
-    String model;
-    double engineVolume;
-    String color;
-    int year;
-    String country;
+    private String brand;
+    private String model;
+    private double engineVolume;
+    private String color;
+    private int year;
+    private String country;
+
+    public Car(String brand, String model, double engineVolume, String color, int year, String country) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.year = year;
+        this.country = country;
+    }
 
     @Override
     public String toString() {
--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        Car carGranta = new Car();
-        carGranta.brand = "Lada";
-        carGranta.model = "Granta";
-        carGranta.color = "yellow";
-        carGranta.country = "Russia";
-        carGranta.engineVolume = 1.7;
-        carGranta.year = 2015;
+        Car carGranta = new Car("Lada", "Granta", 1.7, "yellow", 2015, "Russia");
 
-        Car carA8 = new Car();
-        carA8.brand = "Audi";
-        carA8.model = "A8";
-        carA8.color = "black";
-        carA8.country = "Germany";
-        carA8.engineVolume = 3.0;
-        carA8.year = 2020;
+        Car carA8 = new Car("Audi", "A8", 3.0, "black", 2020, "Germany");
 
-        Car carZ8 = new Car();
-        carZ8.brand = "BMW";
-        carZ8.model = "Z8";
-        carZ8.color = "black";
-        carZ8.country = "Germany";
-        carZ8.engineVolume = 3.0;
-        carZ8.year = 2021;
+        Car carZ8 = new Car("BMW", "Z8", 3.0, "black", 2021, "Germany");
 
-        Car carSportage = new Car();
-        carSportage.brand = "KIA";
-        carSportage.model = "Sportage";
-        carSportage.color = "red";
-        carSportage.country = "South Korea";
-        carSportage.engineVolume = 2.4;
-        carSportage.year = 2016;
+        Car carSportage = new Car("KIA", "Sportage", 2.4, "red", 2016, "South Korea");
 
-        Car carAvante = new Car();
-        carAvante.brand = "Hyundai";
-        carAvante.model = "Avante";
-        carAvante.color = "orange";
-        carAvante.country = "South Korea";
-        carAvante.engineVolume = 1.6;
-        carAvante.year = 2016;
+        Car carAvante = new Car("Hyundai", "Avante", 1.6, "orange", 2016, "South Korea");
 
         System.out.println(carGranta.toString());
         System.out.println(carA8.toString());
